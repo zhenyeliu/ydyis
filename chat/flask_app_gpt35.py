2ab7850c43ed41e75766afb7c6c885bca39d8899
zhenyeliu <liuzhenye@hotmail.com>
2023-03-02 13:45:32 +0800

    Update flask_app_gpt35.py

--- chat/flask_app_gpt35.py
+++ chat/flask_app_gpt35.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.getcwd()))
 from flask import Flask, render_template, request, jsonify
 import time
 import threading
-openai.api_key = "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"
+openai.api_key = "sk-"
 
 def generate_response(prompt):
     try:
